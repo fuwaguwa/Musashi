5c412324e57a382e8c5526863bc7a6b6a50b50eb
flufl <flufftehgamer@gmail.com>
2023-01-30 17:40:16 +0700

    feat: recommendation

--- src/commands/misc/subcommands/musashi/help.ts
+++ src/commands/misc/subcommands/musashi/help.ts
@@ -6,7 +6,7 @@ export = async (interaction: ChatInputCommandInteraction) =>
 		.setColor("#2f3136")
 		.setTitle("How To Setup Musashi")
 		.setDescription(
-			`1. Create a channel dedicated to chatting with her. It is recommended that you disable all other bots and commands in the channel for the best exprience!\n\n` +
+			`1. Create a channel dedicated to chatting with her. It is recommended that you disable all other bots and commands in the channel for the best exprience as well as **add at least 5 seconds of slowmode to the channel to avoid spamming**!\n\n` +
 				`2. Run \`/set-chat-channel\` and pick the channel you have created.\n\n` +
 				`3. You can now talk with Mushashi in the channel! It is also recommended form intelligible sentences for the best results!`
 		);
