7b3f4adfd70cb998bfe7490a5e7a6efc2b020b77
flufl <flufftehgamer@gmail.com>
2023-01-30 16:30:02 +0700

    feat: activity

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,9 +1,15 @@
 import { Event } from "../structures/Event";
 import fetch from "node-fetch";
+import { client } from "..";
+import { ActivityType } from "discord.js";
 
 export default new Event("ready", () => 
 {
 	console.log("Musashi is ready!");
+	client.user.setActivity({
+		name: "shikikans' needs",
+		type: ActivityType.Listening,
+	});
 
 	fetch(
 		`https://api-inference.huggingface.co/models/Fuwaguwa/DialoGPT-Medium-AzurLaneMusashi-v${process.env.version}`,
